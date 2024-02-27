1994a66885ff8c830d1ce46c1798626db7c3f3b6
Wan Sim <0420syj@naver.com>
2024-02-28 01:06:42 +0900

    feat: add storybook

--- packages/shared-ui/.eslintrc.js
+++ packages/shared-ui/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["@repo/eslint-config/react.js", "plugin:storybook/recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 };
