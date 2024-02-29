947f0d3fd371bf9a835566ca191100d701bbae80
Wan Sim <0420syj@naver.com>
2024-02-29 23:41:52 +0900

    chore: change utils package name

--- apps/moomin-money/src/components/home/home-page.tsx
+++ apps/moomin-money/src/components/home/home-page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@repo/ui";
+import { Button, Input } from "@repo/ui";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
       <Button>Click me</Button>
+      <Input type="text" placeholder="Type something" />
     </div>
   );
 };
