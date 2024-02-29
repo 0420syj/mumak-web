d18ce377a2ead2680e98d3cc600c7255bbbb13a6
Wan Sim <0420syj@naver.com>
2024-02-29 21:13:12 +0900

    chore: delete shared-ui package

--- apps/moomin-money/src/components/home/home-page.tsx
+++ apps/moomin-money/src/components/home/home-page.tsx
@@ -1,19 +1,7 @@
-import { Button } from "@repo/shared-ui";
-
 const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
-      <div className="flex flex-row space-x-4">
-        <Button label="Click me" size="large" primary />
-        <Button label="Click me" size="medium" primary />
-        <Button label="Click me" size="small" primary />
-      </div>
-      <div className="flex flex-row space-x-4">
-        <Button label="Click me" size="large" />
-        <Button label="Click me" size="medium" />
-        <Button label="Click me" size="small" />
-      </div>
     </div>
   );
 };
