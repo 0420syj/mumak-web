21748f672fb9dd1100e90892aa6ebdfd9e0e830b
Wan Sim <0420syj@naver.com>
2024-03-01 03:22:38 +0900

    feat: add components
     -label
     -avatar
     -calendar
     -card
     -popover
     -progress
     -radio-group
     -toast
     -table
     -skeleton
     -form

---
+++ packages/ui/src/components/ui/button/button.stories.tsx
@@ -0,0 +1,100 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "@ui/components/ui/button";
+
+import {
+  ChevronRightIcon,
+  EnvelopeOpenIcon,
+  ReloadIcon,
+} from "@radix-ui/react-icons";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/UI/Button",
+  component: Button,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Secondary",
+    variant: "secondary",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    children: "Destructive",
+    variant: "destructive",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Outline",
+    variant: "outline",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: "Ghost",
+    variant: "ghost",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    children: "Link",
+    variant: "link",
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    variant: "outline",
+    size: "icon",
+    children: <ChevronRightIcon className="h-4 w-4" />,
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    children: (
+      <>
+        <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+      </>
+    ),
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    disabled: true,
+    children: (
+      <>
+        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </>
+    ),
+  },
+};
+
+export const AsChild: Story = {
+  args: {
+    asChild: true,
+    children: (
+      <a href="#" onClick={(e) => e.preventDefault()}>
+        Login
+      </a>
+    ),
+  },
+};
--- packages/ui/src/components/ui/index.ts
+++ packages/ui/src/components/ui/index.ts
@@ -1,3 +1,16 @@
 export * from "./button";
 export * from "./input";
 export * from "./dropdown-menu";
+export * from "./label";
+
+export * from "./avatar";
+export * from "./calendar";
+export * from "./card";
+export * from "./popover";
+export * from "./progress";
+export * from "./radio-group";
+
+export * from "./toast";
+export * from "./table";
+export * from "./skeleton";
+export * from "./form";
