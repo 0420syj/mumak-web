eaa931ce0b222f92b82d8cf7e934e99456fe4bed
Wan Sim <0420syj@naver.com>
2024-02-29 22:54:46 +0900

    feat: init shadcn/ui

--- packages/new-ui/src/App.tsx
+++ packages/new-ui/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="bg-glow-conic">Vite + React</h1>;
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
