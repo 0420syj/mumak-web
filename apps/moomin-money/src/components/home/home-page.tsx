e6b813b832ae10011224e3b4d2e4ddc62d50004a
Wan Sim <0420syj@naver.com>
2024-03-04 17:35:51 +0900

    Revert "feat: Home Page (#7)"
    This reverts commit 736a583f40f8fee02f88bd764394104701da45dd.

--- apps/moomin-money/src/components/home/home-page.tsx
+++ apps/moomin-money/src/components/home/home-page.tsx
@@ -1,38 +1,7 @@
-import dynamic from "next/dynamic";
-
-import { Skeleton } from "@repo/ui/skeleton";
-import { HomeForm } from "@moomin-money/components/home/home-form";
-
-const MoneySpendBoard = dynamic(
-  () => import("@moomin-money/components/money-spend-board"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex flex-col w-full mb-4">
-        <div className="flex flex-row justify-around">
-          <div className="flex flex-col items-center">
-            <Skeleton className="w-[60px] h-[24px]" />
-            <Skeleton className="w-[64px] h-[20px]" />
-          </div>
-          <div className="flex flex-col items-center">
-            <Skeleton className="w-[60px] h-[24px]" />
-            <Skeleton className="w-[64px] h-[20px]" />
-          </div>
-          <div className="flex flex-col items-center">
-            <Skeleton className="w-[60px] h-[24px]" />
-            <Skeleton className="w-[64px] h-[20px]" />
-          </div>
-        </div>
-      </div>
-    ),
-  }
-);
-
 const HomePage = () => {
   return (
-    <div className="container items-center">
-      <MoneySpendBoard />
-      <HomeForm />
+    <div>
+      <h1>Home</h1>
     </div>
   );
 };
