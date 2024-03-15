d8f4152ae2e57235323d5f0749909f2613f953a6
Wan Sim <0420syj@naver.com>
2024-03-16 05:15:48 +0900

    feat: init default name value

--- apps/moomin-money/src/components/home/home-page.tsx
+++ apps/moomin-money/src/components/home/home-page.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion -- safe */
+
 import dynamic from 'next/dynamic';
 import { Skeleton } from '@repo/ui/skeleton';
+import { getServerSession } from 'next-auth';
 import { HomeForm } from '@moomin-money/components/home/home-form';
+import { authOptions } from '@moomin-money/libs/auth';
 
 const MoneySpendBoard = dynamic(() => import('@moomin-money/components/money-spend-board'), {
   ssr: false,
@@ -22,7 +26,16 @@ const MoneySpendBoard = dynamic(() => import('@moomin-money/components/money-spe
   ),
 });
 
-function HomePage(): React.ReactElement {
+async function HomePage(): Promise<React.ReactElement> {
+  const session = await getServerSession(authOptions);
+
+  const allowedAccountList: string[] = process.env.GOOGLE_ACCOUNT_LIST!.split(',');
+
+  const nameMap: Record<string, 'wanny' | 'moomin'> = {
+    [allowedAccountList[0]]: 'wanny',
+    [allowedAccountList[1]]: 'moomin',
+  };
+
   return (
     <div className="container items-center space-y-8">
       <section>
@@ -30,7 +43,7 @@ function HomePage(): React.ReactElement {
       </section>
       <HomeForm
         defaultValues={{
-          name: 'wanny',
+          name: session?.user?.email ? nameMap[session.user.email] : undefined,
         }}
       />
     </div>
