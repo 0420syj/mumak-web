b12599ced6d865c3149c5138b886d7d2ccbc9da3
Wan Sim <0420syj@naver.com>
2024-03-17 03:57:59 +0900

    feat: add moneybook page metadata

--- apps/moomin-money/src/app/moneybook/page.tsx
+++ apps/moomin-money/src/app/moneybook/page.tsx
@@ -1,1 +1,14 @@
+import type { Metadata, ResolvingMetadata } from 'next';
+
+export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `Moneybook`,
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
+}
+
 export { default } from '@moomin-money/components/moneybook/moneybook-page';
