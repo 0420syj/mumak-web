57250609ad46d5b4d4a163654ac19e81d8830627
Wan Sim <0420syj@naver.com>
2024-03-17 07:16:03 +0900

    fix: type error in ResolvingMetadata

--- apps/moomin-money/src/app/moneybook/page.tsx
+++ apps/moomin-money/src/app/moneybook/page.tsx
@@ -1,14 +1,7 @@
-import type { Metadata, ResolvingMetadata } from 'next';
+import type { Metadata } from 'next';
 
-export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: `Moneybook`,
-    openGraph: {
-      images: [...previousImages],
-    },
-  };
-}
+export const metadata: Metadata = {
+  title: 'Moneybook',
+};
 
 export { default } from '@moomin-money/components/moneybook/moneybook-page';
