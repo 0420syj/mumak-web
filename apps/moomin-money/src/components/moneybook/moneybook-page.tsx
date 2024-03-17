1db3914987ac111843d32be7c6dd8eaaa412f296
Wan Sim <0420syj@naver.com>
2024-03-17 19:19:26 +0900

    feat: add tab in money book page

--- apps/moomin-money/src/components/moneybook/moneybook-page.tsx
+++ apps/moomin-money/src/components/moneybook/moneybook-page.tsx
@@ -1,9 +1,24 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@repo/ui/tabs';
+import { getUserName } from '@moomin-money/libs/auth';
 import DataTableContainer from './block/data-table/data-table-container';
 
-function MoneybookPage(): React.ReactElement {
+async function MoneybookPage(): Promise<React.ReactElement> {
+  const name = await getUserName('wanny');
+
   return (
     <div className="container items-center space-y-8">
-      <DataTableContainer name="wanny" />
+      <Tabs defaultValue={name}>
+        <TabsList className="flex gap-4">
+          <TabsTrigger value="wanny">🐶 빵떡</TabsTrigger>
+          <TabsTrigger value="moomin">🐻‍❄️ 무민</TabsTrigger>
+        </TabsList>
+        <TabsContent value="wanny">
+          <DataTableContainer name="wanny" />
+        </TabsContent>
+        <TabsContent value="moomin">
+          <DataTableContainer name="moomin" />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
