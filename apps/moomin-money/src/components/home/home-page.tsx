1483a92d64a1c21e4dde7145c75dadaf94660f90
Wan Sim <0420syj@naver.com>
2024-03-22 15:36:51 +0900

    feat: add money spend board on money book page

--- apps/moomin-money/src/components/home/home-page.tsx
+++ apps/moomin-money/src/components/home/home-page.tsx
@@ -1,30 +1,11 @@
 import dynamic from 'next/dynamic';
-import { Skeleton } from '@repo/ui/skeleton';
 import { HomeForm } from '@moomin-money/components/home/home-form';
 import { getUserName } from '@moomin-money/libs/auth';
-
-function SkeletonLoading(): React.ReactElement {
-  return (
-    <div className="flex flex-row justify-around">
-      <div className="flex flex-col items-center gap-1">
-        <Skeleton className="w-[60px] h-[28px]" />
-        <Skeleton className="w-[64px] h-[24px]" />
-      </div>
-      <div className="flex flex-col items-center gap-1">
-        <Skeleton className="w-[60px] h-[28px]" />
-        <Skeleton className="w-[64px] h-[24px]" />
-      </div>
-      <div className="flex flex-col items-center gap-1">
-        <Skeleton className="w-[60px] h-[28px]" />
-        <Skeleton className="w-[64px] h-[24px]" />
-      </div>
-    </div>
-  );
-}
+import { MoneySpendBoardSkeleton } from '@moomin-money/components/money-spend-board';
 
 const MoneySpendBoard = dynamic(() => import('@moomin-money/components/money-spend-board'), {
   ssr: false,
-  loading: () => <SkeletonLoading />,
+  loading: () => <MoneySpendBoardSkeleton />,
 });
 
 async function HomePage(): Promise<React.ReactElement> {
--- apps/moomin-money/src/components/moneybook/moneybook-page.tsx
+++ apps/moomin-money/src/components/moneybook/moneybook-page.tsx
@@ -1,12 +1,25 @@
+import dynamic from 'next/dynamic';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@repo/ui/tabs';
 import { getUserName } from '@moomin-money/libs/auth';
-import DataTableContainer from './block/data-table/data-table-container';
+import { MoneySpendBoardSkeleton } from '@moomin-money/components/money-spend-board';
+
+const DataTableContainer = dynamic(() => import('./block/data-table/data-table-container'), {
+  ssr: false,
+});
+
+const MoneySpendBoard = dynamic(() => import('@moomin-money/components/money-spend-board'), {
+  ssr: false,
+  loading: () => <MoneySpendBoardSkeleton />,
+});
 
 async function MoneybookPage(): Promise<React.ReactElement> {
   const name = await getUserName('wanny');
 
   return (
     <div className="container items-center space-y-8">
+      <section>
+        <MoneySpendBoard />
+      </section>
       <Tabs defaultValue={name}>
         <TabsList className="flex gap-4">
           <TabsTrigger className="w-full" value="wanny">
