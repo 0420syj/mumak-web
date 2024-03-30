1cac530acd1d129f791a7cf21472f1859a7fb82c
Wan Sim <0420syj@naver.com>
2024-03-31 02:40:01 +0900

    fix: loading data only once

--- apps/moomin-money/src/components/home/block/note-form-field.tsx
+++ apps/moomin-money/src/components/home/block/note-form-field.tsx
@@ -12,14 +12,18 @@ import { includeByCho } from '@moomin-money/libs/search';
 import type { formSchema } from '../home-form';
 
 export function NoteFormField({ control }: { control: Control<z.infer<typeof formSchema>> }): React.ReactElement {
-  const getAutoComplete = async (value: string): Promise<(string | number)[]> => {
+  const fetchNoteData = async (name: string): Promise<(string | number)[]> => {
     const session = await getSession();
     if (!session) {
       return [];
     }
 
     if (!process.env.NEXT_PUBLIC_GOOGLE_WANNY_SHEET_NAME || !process.env.NEXT_PUBLIC_GOOGLE_MOOMIN_SHEET_NAME) {
-      throw new Error('GOOGLE_WANNY_SHEET_NAME or GOOGLE_MOOMIN_SHEET_NAME is not set');
+      throw new Error('NEXT_PUBLIC_GOOGLE_WANNY_SHEET_NAME or NEXT_PUBLIC_GOOGLE_MOOMIN_SHEET_NAME is not set');
+    }
+
+    if (!process.env.NEXT_PUBLIC_GOOGLE_NOTE_RANGE) {
+      throw new Error('NEXT_PUBLIC_GOOGLE_NOTE_RANGE is not set');
     }
 
     const sheetNameMap: Record<string, string> = {
@@ -27,28 +31,38 @@ export function NoteFormField({ control }: { control: Control<z.infer<typeof for
       moomin: process.env.NEXT_PUBLIC_GOOGLE_MOOMIN_SHEET_NAME,
     };
 
-    const name = control._defaultValues.name!;
     const sheetName = !isVercelEnvProduction() ? process.env.NEXT_PUBLIC_GOOGLE_TEST_SHEET_NAME! : sheetNameMap[name];
 
-    const response = await getSheetValues({ sheetName, range: process.env.NEXT_PUBLIC_GOOGLE_NOTE_RANGE });
-    const filteredResponse = response
-      .filter(item => {
-        const [note] = item;
-        if (typeof note !== 'string') {
-          return false;
-        }
-        return includeByCho(value, note);
-      })
-      .map(item => {
-        const [note] = item;
-        return note;
-      });
-
-    setAutoComplete(filteredResponse);
-    return filteredResponse;
+    const response = await getSheetValues({ sheetName, range: process.env.NEXT_PUBLIC_GOOGLE_NOTE_RANGE }).then(
+      values => {
+        return values
+          .map(item => {
+            const [note] = item;
+            return note;
+          })
+          .filter((item, index, self) => self.indexOf(item) === index)
+          .filter(item => item !== '');
+      }
+    );
+    return response;
   };
 
   const [autoComplete, setAutoComplete] = React.useState<(string | number)[]>([]);
+  const [searchValue, setSearchValue] = React.useState<string>('');
+
+  React.useEffect(() => {
+    void fetchNoteData(control._defaultValues.name!).then(response => {
+      setAutoComplete(response);
+    });
+  }, [control._defaultValues.name, setAutoComplete]);
+
+  const handleSearch = React.useCallback((value: string): void => {
+    setSearchValue(value);
+  }, []);
+
+  const filteredAutoComplete = React.useMemo(() => {
+    return autoComplete.filter(item => includeByCho(searchValue, String(item)));
+  }, [autoComplete, searchValue]);
 
   return (
     <FormField
@@ -62,15 +76,32 @@ export function NoteFormField({ control }: { control: Control<z.infer<typeof for
               {...field}
               className="text-base h-10"
               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                void getAutoComplete(event.target.value);
+                handleSearch(event.target.value);
                 field.onChange(event.target.value);
               }}
             />
           </FormControl>
-          <ul className="max-h-[300px] overflow-y-auto overflow-x-hidden">
-            {autoComplete.map((item, index) => (
-              // eslint-disable-next-line react/no-array-index-key -- safe
-              <li key={index}>{item}</li>
+          <ul className="max-h-[100px] overflow-y-auto overflow-x-hidden rounded-md border border-foreground/20">
+            {filteredAutoComplete.map(item => (
+              <li
+                className="text-base p-2 cursor-pointer hover:bg-foreground/10"
+                key={`list-${item}`}
+                onClick={() => {
+                  field.onChange(item);
+                  handleSearch(item as string);
+                }}
+                onKeyDown={event => {
+                  if (event.key === 'Enter' || event.key === ' ') {
+                    field.onChange(item);
+                    handleSearch(item as string);
+                  }
+                }}
+                // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role -- safe
+                role="button"
+                tabIndex={-1} // Added tabIndex of -1
+              >
+                {item}
+              </li>
             ))}
           </ul>
           <FormMessage />
