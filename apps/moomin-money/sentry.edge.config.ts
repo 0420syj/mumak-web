deae6b4a24374932f6eaa496d22a8fef7ab764e7
Wan Sim <0420syj@naver.com>
2024-04-05 01:56:02 +0900

    feat: install sentry

---
+++ apps/moomin-money/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://588f1acc826accfa6b9e04aeaef93d8a@o4507026620088320.ingest.us.sentry.io/4507026624217088",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
