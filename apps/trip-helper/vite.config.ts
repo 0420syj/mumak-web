87d4749e0233bb9711fe4f40a6446ff45ba7a2b0
Wan Sim <0420syj@naver.com>
2024-04-06 00:32:56 +0900

    feat: init vitest for trip-helper

--- apps/trip-helper/vite.config.ts
+++ apps/trip-helper/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -9,5 +11,10 @@ export default defineConfig({
     alias: {
       '@trip-helper': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
   },
 });
